8a36be46a3bbe0ba8597d9e955ba5940568430d4
renugajagadeesan <45448474+renugajagadeesan@users.noreply.github.com>
2024-12-12 19:02:46 +0530

    Update server.js
    updated today

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const contentSchema = new mongoose.Schema({
     },
 });
 //hello
-//worldgit
+//world
 // Create Content Model
 const Content = mongoose.model('Content', contentSchema);
 // Cloudinary Configuration
